ea0411d472cf8cd60fbc645cf3a9cb593561b640
solangealmn <solangealmn@gmail.com>
2018-08-05 18:39:17 -0300

    inclusão do modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Add your JavaScript
-// Geolocation pede permisão para saber sua localização.
+// Carregar o mapa com Geolocation.
 var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
@@ -7,7 +7,6 @@ function initMap() {
     zoom: 16
   });
   infoWindow = new google.maps.InfoWindow;
-
   // Iserir localização de todos os restuarantes
   for (var i = 0; i < restaurantes.length; i++) {
     var marker = new google.maps.Marker({
@@ -15,7 +14,6 @@ function initMap() {
       title: restaurantes[i].name,
       map: map
     });
-
     var infowindow = new google.maps.InfoWindow(), marker;
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
       return function() {
@@ -24,15 +22,13 @@ function initMap() {
       }
     })(marker))
   }
-
-  // Try HTML5 geolocation.
+  //HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-
       infoWindow.setPosition(pos);
       infoWindow.setContent('Você está aqui.');
       infoWindow.open(map);
@@ -45,43 +41,43 @@ function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 }
-
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.setPosition(pos);
   infoWindow.setContent(browserHasGeolocation ?
     'Error: O serviço de Geolocalização falhou.' :
     'Error: Seu browser não tem suporte ao serviço de Geolocalização.');
-    infoWindow.open(map);
-  }
+  infoWindow.open(map);
+}
 
-  function imgFiltered(images) {
-    $( '#food-thumbs' ).html('');
-    images.map(function(images) {
-      $("#food-thumbs").append('<li class="open-modal"><p>' + images.name +'</p><img src="' + images.image + '"></li>');
-    });
-  }
-
-  //Splash screen
-  $(document).ready( ()=> {
-    //Efeito da tela inicial
-    $('.lg-foodmap').delay('1000').fadeToggle('slow', 'linear');
-    $('.screen-main').delay('5000').fadeIn('slow');
-    $('.lg-foodmap').delay('3000').fadeOut('slow');
-
-    //Monta a lista de imagens
-    function imageList() {
-      for (var i = 0; i < restaurantes.length; i++) {
-        $("#food-thumbs").append('<li class="open-modal"><p>' + restaurantes[i].name +'</p><img src="'+restaurantes[i].image + '"></li>');
-      }
+$(document).ready( ()=> {
+  //Efeito da tela inicial
+  $('.lg-foodmap').delay('1000').fadeToggle('slow', 'linear');
+  $('.screen-main').delay('5000').fadeIn('slow');
+  $('.lg-foodmap').delay('3000').fadeOut('slow');
+  //Monta a lista de imagens
+  function imageList() {
+    for (var eatery of restaurantes) {
+      $("#food-thumbs").append('<li class="item-foodmap" data-toggle="modal" data-target="#open-modal"><p>' + eatery.name + '</p><img src="' + eatery.image +'" alt="' + eatery.name + '"></li>');
     }
-    imageList();
-    $
-    //Filtro de restaurantes
-    $('#search').click(()=> {
-      var itemValue = $('.search-item').val();
-      var eateryFiltered = restaurantes.filter(eatery => itemValue === eatery.name || itemValue === eatery.type);
-      //console.log(eateryFiltered);
-      imgFiltered(eateryFiltered);
+  }
+  imageList();
+  //Filtro de restaurantes
+  $('#search').click( ()=> {
+    var itemValue = $('.search-item').val();
+    var eateryFiltered = restaurantes.filter(eatery => itemValue === eatery.name || itemValue === eatery.type);
+    //console.log(eateryFiltered);
+    imgFiltered(eateryFiltered);
+  })
+  function imgFiltered(images) {
+    $('#food-thumbs').html('');
+    images.map((images)=> {
+      $("#food-thumbs").append('<li class="item-foodmap" data-toggle="modal" data-target="#open-modal"><p>' + images.name + '</p><img src="' + images.image +'" alt="' + images.name + '"></li>');
     });
-
-  });
+  }
+  //Conteúdo dinamico do modal
+    $(".itemFoodmap").click( ()=> {
+      $.each(restaurantes, (index, value) => {
+        $('.modal-body').html('<h3 class="bg-foodmap text-white p-3 m-0">' + restaurantes.name + '</h3><img src="' + restaurantes.image + '" alt="' + restaurantes.image + '"><p class="p-3 m-0">' + restaurantes.description + '</p>');
+      });
+    });
+});
